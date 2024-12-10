22c2630fffcb6341a4a7455b39218610d3ca8d5b
ivaiva7 <v7iva7@gmail.com>
2024-12-10 21:26:28 +0300

    вопрос

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -1,86 +1,9 @@
-const apiUrl = Cypress.env('BURGER_API_URL');
 describe('Burger Constructor', () => {
   beforeEach(() => {
-    cy.setCookie(
-      'accessToken',
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
-    );
-    cy.setCookie(
-      'refreshToken',
-      'c854c28fdc6accc55e2e1324dbb76c2e6e27e306ac26fdda33b31d606710566f5c57496620fc9185'
-    );
-
-    cy.intercept('GET', '/auth/user', {
-      statusCode: 200,
-      body: {
-        id: '1',
-        email: 'test@example.com',
-        name: 'Test User',
-        accessToken: 'mock-access-token'
-      }
-    }).as('checkAuth');
-
-    cy.intercept('POST', `${apiUrl}/orders`, {
-      statusCode: 200,
-      headers: {
-        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
-      },
-      body: {
-        success: true,
-        name: 'Краторный био-марсианский бургер',
-        order: {
-          ingredients: [
-            {
-              _id: '643d69a5c3f7b9001cfa093c',
-              name: 'Краторная булка N-200i',
-              type: 'bun',
-              proteins: 80,
-              fat: 24,
-              carbohydrates: 53,
-              calories: 420,
-              price: 1255,
-              image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-              image_mobile:
-                'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-              image_large:
-                'https://code.s3.yandex.net/react/code/bun-02-large.png',
-              __v: 0
-            },
-            {
-              _id: '643d69a5c3f7b9001cfa0941',
-              name: 'Биокотлета из марсианской Магнолии',
-              type: 'main',
-              proteins: 420,
-              fat: 142,
-              carbohydrates: 242,
-              calories: 4242,
-              price: 424,
-              image: 'https://code.s3.yandex.net/react/code/meat-01.png',
-              image_mobile:
-                'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
-              image_large:
-                'https://code.s3.yandex.net/react/code/meat-01-large.png',
-              __v: 0
-            }
-          ],
-          _id: '1',
-          owner: {
-            name: 'Test User',
-            email: 'test@example.com',
-            createdAt: '2024-09-21T20:44:30.309Z',
-            updatedAt: '2024-10-06T05:04:41.136Z'
-          },
-          status: 'done',
-          name: 'Краторный био-марсианский бургер',
-          createdAt: '2024-12-09T18:53:01.122Z',
-          updatedAt: '2024-12-09T18:53:01.983Z',
-          number: 62088,
-          price: 1679
-        }
-      }
-    }).as('createOrder');
-
-    cy.visit('localhost:4000/');
+    cy.intercept('GET', 'api/ingredients', {
+      fixture: 'ingredients.json'
+    });
+    cy.visit('http://localhost:4000');
   });
 
   it('Добавление ингредиента в конструктор', () => {
@@ -102,12 +25,26 @@ describe('Burger Constructor', () => {
     cy.get('[data-testid="modal"]').should('exist');
     cy.get('[data-testid="modal-close"]').click();
   });
+});
+
+describe('оформление заказа работает корректно', function () {
+  beforeEach(function () {
+    cy.intercept('GET', 'api/ingredients', {
+      fixture: 'ingredients.json'
+    });
+    cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+    cy.intercept('POST', 'api/orders', { fixture: 'post-order.json' }).as(
+      'postOrder'
+    );
+    window.localStorage.setItem(
+      'refreshToken',
+      JSON.stringify('test-refreshToken')
+    );
+    cy.setCookie('accessToken', 'test-accessToken');
+    cy.visit('http://localhost:4000');
+  });
 
   it('Оформление заказа', () => {
-    cy.wait('@checkAuth');
-    cy.getCookie('accessToken');
-    cy.getCookie('refreshToken');
-    cy.url().should('not.include', '/login');
     cy.get('[data-testid="ingredient-item"]')
       .should('exist')
       .first()
@@ -121,11 +58,22 @@ describe('Burger Constructor', () => {
         cy.contains('button', 'Добавить').click();
       });
     cy.get('[data-testid="order-button"]').click();
-    //здесь ошибка 403 при создании заказа, не понимаю как исправить
-    cy.wait('@createOrder', { timeout: 20000 });
+    cy.wait('@postOrder')
+      .its('request.body')
+      .should('deep.equal', {
+        ingredients: [
+          'Краторная булка N-200i',
+          'Биокотлета из марсианской Магнолии'
+        ]
+      });
     cy.get('[data-testid="modal"]').should('exist');
     cy.get('[data-testid="order-details-ui"]').should('exist');
     cy.get('[data-testid="modal-close"]').click();
     cy.get('[data-testid="constructor-ingredient"]').should('have.length', 0);
+  });
+
+  afterEach(function () {
+    cy.clearLocalStorage();
+    cy.clearCookies();
   });
 });
